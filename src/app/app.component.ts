777e996a31377b7fb9f718465a01f07d14c6af0d
predragruzic <148483600+predragruzic@users.noreply.github.com>
2026-01-08 18:58:48 +0000

    work on course list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CoursesListComponent } from "./courses-list/courses-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CoursesListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
